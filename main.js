543a2a83031e0820582bac6bf7a3eb29e5004112
Luis Mario Ramirez Muñoz <limao052106@outlook.com>
2024-08-30 08:34:42 -0500

    chore: Update HTML structure and add search button functionality

--- main.js
+++ main.js
@@ -1,39 +1,51 @@
 let resultado = document.getElementById("wrap-result");
 let buscarbtn = document.getElementById("buscar-button");
 let url = "https://thecocktaildb.com/api/json/v1/1/search.php?s="
+
 let getInfo = () => {
+
     let input = document.getElementById("buscar").value;
+
     if (input != "") {
         fetch(url + input)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            console.log(data.drinks[0]);
-            let tragos = data.drinks[0];
-            console.log(tragos.idDrink);
-            console.log(tragos.strDrink);
-            console.log(tragos.strCategory);
-            console.log(tragos.strDrinkThumb);
-            console.log(tragos.strInstructions);
-            let contador = 1;
-            let ingredientes = [];
-            for (let i in tragos){
-                let ingrediente = "";
-                let medida = "";
-                if (i.startsWith("strIngredient") && tragos[i]){
-                    ingrediente = tragos[i];
-                    if(tragos["strMeasure" + contador]){
-                        medida = tragos["strMeasure" + contador];
+            .then((response) => response.json())
+            .then((data) => {
+
+                console.log(data);
+                console.log(data.drinks[0]);
+
+                let tragos = data.drinks[0];
+                
+                console.log(tragos.idDrink);
+                console.log(tragos.strDrink);
+                console.log(tragos.strCategory);
+                console.log(tragos.strDrinkThumb);
+                console.log(tragos.strInstructions);
+
+                let contador = 1;
+                let ingredientes = [];
+                
+                for (let i in tragos) {
+
+                    let ingrediente = "";
+                    let medida = "";
+
+                    if (i.startsWith("strIngredient") && tragos[i]) {
+                        ingrediente = tragos[i];
+                        if (tragos["strMeasure" + contador]) {
+                            medida = tragos["strMeasure" + contador];
+                            contador++;
+                        } else {
+                            medida = "";
+                        }
                         contador++;
-                    }else{
-                        medida = "";
+                        ingredientes.push(`${medida} ${ingrediente}`);
                     }
-                    contador++;
-                    ingredientes.push(`${medida} ${ingrediente}`);
                 }
-            }
-            console.log(ingredientes);
-            resultado.innerHTML = `
+
+                console.log(ingredientes);
+
+                resultado.innerHTML = `
                 <img src= ${tragos.strDrinkThumb}>
                 <h2>${tragos.strDrink}</h2>
                 <h3>ID: ${tragos.idDrink}</h3>
@@ -44,18 +56,20 @@ let getInfo = () => {
                 <h3>Instrucciones:</h3>
                 <p>${tragos.strInstructions}</p>
             `;
-            let ingredientesUL = document.querySelector(".ingredientes");
-            ingredientes.forEach((ingrediente) => {
-                let li = document.createElement("li");
-                li.innerHTML = ingrediente;
-                ingredientesUL.appendChild(li);
+                let ingredientesUL = document.querySelector(".ingredientes");
+                ingredientes.forEach((ingrediente) => {
+                    let li = document.createElement("li");
+                    li.innerHTML = ingrediente;
+                    ingredientesUL.appendChild(li);
+                });
             });
-        });
     }
 };
+
 window.addEventListener("load", () => {
     document.getElementById("buscar").value = "";
     getInfo();
 });
+
 buscarbtn.addEventListener("click", getInfo);
 
