8ca3a49f8838c504cfc91229a4da2e418dd873d6
Luis Mario Ramirez Muñoz <limao052106@outlook.com>
2024-08-31 14:43:13 -0500

    Funcionalidad botones favoritos
    Falta mostrar por pantalla la informacion pero del resto todo funcional

--- main.js
+++ main.js
@@ -1,22 +1,65 @@
 let resultado = document.getElementById("wrap-result");
 let buscarbtn = document.getElementById("buscar-button");
 let favbtn = document.getElementById("favoritos");
+let Afavoritos = document.getElementById("Afavoritos");
 let fav = document.getElementById("wrap-fav");
+const menuFavoritos = document.querySelector('.menuFavoritos');
 let url = "https://thecocktaildb.com/api/json/v1/1/search.php?s="
+let tragosFavoritos = [];
+let contadorClic = 0;
+let contadorClicA = 0;
 
-let tragos
-/*hay que desactivar el boton de mostrar favoritos hasta que exista al menos un elemento seleccionado como favorito
-function showfav() {
-    console.log("click");
-    favbtn.style.color = "yellow";
-
-
-    fav.innerHTML = `      
-                <h2>${tragos.strDrink}</h2>
-                <h3>ID: ${tragos.idDrink}</h3>
+function showfav(tragosFavoritos) {
+    if (contadorClic == 0) {
+        favbtn.style.color = "yellow";
+        contadorClic++;
+        console.log("contadorClic: " + contadorClic);
+    }else if (contadorClic == 1) {
+        favbtn.style.color = "white";
+        contadorClic--;
+        console.log("contadorClic: " + contadorClic);
+        console.log("Se cerro tragos favoritos");
+    }
+    
+    if (menuFavoritos.hasAttribute('hidden')) {
+        menuFavoritos.removeAttribute('hidden'); 
+    } else {
+        menuFavoritos.setAttribute('hidden', ''); 
+    }
+    
+    console.log("Se abrio tragos favoritos");
+    
+tragosFavoritos.forEach((trago) => {
+    fetch(url + trago)
+        .then((response) => response.json())
+        .then((data) => {
+            let bebida = data.drinks[0];
+            //Aqui pueden manejar como quieran la informacion de la bebida no pude ponerla por pantalla
+            console.log("El nombre de la bebida es: " + bebida.strDrink);
+            console.log("El id de la bebida es: " + bebida.idDrink);
+            fav.innerHTML = `      
+                <h2 id="nombreFav">${bebida.strDrink}</h2>
+                <h3 id="idFav">ID: ${bebida.idDrink}</h3>
             `;
+        })
+        .catch(error => {
+            console.log("error");
+            fav.innerHTML = `
+                <h2 id="nombreFav">Carrito vacio</h2>
+            `;
+        });
+    });
 }
-*/
+
+function agregarFav(dato) {
+    if (!tragosFavoritos.includes(dato)) {
+        tragosFavoritos.push(dato);
+        console.log("Se agrego el trago a favoritos");
+        console.log("Los tragos favoritos son: " + tragosFavoritos);
+    } else {
+        console.log("Este trago ya esta en tus favoritos");
+    }
+}
 
 let getInfo = () => {
 
@@ -31,7 +74,7 @@ let getInfo = () => {
                 console.log(data.drinks[0]);
 
                 let tragos = data.drinks[0];
-
+                
                 console.log(tragos.idDrink);
                 console.log(tragos.strDrink);
                 console.log(tragos.strCategory);
@@ -46,7 +89,7 @@ let getInfo = () => {
                     let ingrediente = "";
                     let medida = "";
 
-                    if (i.startsWith("strIngredient") && tragos[i]) {
+                    if (i.startsWith("strIngredient") && tragos[i])      {
                         ingrediente = tragos[i];
                         if (tragos["strMeasure" + contador]) {
                             medida = tragos["strMeasure" + contador];
@@ -61,9 +104,17 @@ let getInfo = () => {
 
                 console.log(ingredientes);
 
+                console.log(tragos.strDrink);
+
                 resultado.innerHTML = `
                 <img src= ${tragos.strDrinkThumb}>
-                <h2>${tragos.strDrink}</h2>
+                <h2>${tragos.strDrink} 
+                <button id="Afavoritos" onclick="agregarFav('${tragos.strDrink}')">
+                    <span class="material-symbols-outlined">
+                        star
+                    </span>
+                </button>
+                </h2>
                 <h3>ID: ${tragos.idDrink}</h3>
                 <h3>Categoria: </h3>
                 <p>${tragos.strCategory}</p>
@@ -78,7 +129,7 @@ let getInfo = () => {
                     li.innerHTML = ingrediente;
                     ingredientesUL.appendChild(li);
                 });
-                
+
             });
     }
 };
